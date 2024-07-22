5b0d8d78561ee65e838df829b50723e9f3225c02
Reddyong <d0209hong@naver.com>
2024-07-22 12:31:20 +0900

    #1 - sns 프로젝트 서비스 기본 환경설정 세팅 완료

---
+++ src/test/java/com/fc8/snsproject/SnsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc8.snsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
