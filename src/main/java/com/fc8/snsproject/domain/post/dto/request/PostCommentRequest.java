0ee13eede72f80896d9aa133978a5421fcd6eb82
Reddyong <d0209hong@naver.com>
2024-07-29 18:33:38 +0900

    #26 - 댓글 엔티티 및 dto 들 생성
    댓글 저장 및 조회를 위한 엔티티 생성.
    댓글 저장, 조회를 위한 request, response, dto 생성.

--- src/main/java/com/fc8/snsproject/domain/post/dto/request/PostCommentRequest.java
+++ src/main/java/com/fc8/snsproject/domain/post/dto/request/PostCommentRequest.java
@@ -1,9 +1,9 @@
 package com.fc8.snsproject.domain.post.dto.request;
 
 public record PostCommentRequest(
-        String comment
+        String content
 ) {
-    public static PostCommentRequest of(String comment) {
-        return new PostCommentRequest(comment);
+    public static PostCommentRequest of(String content) {
+        return new PostCommentRequest(content);
     }
 }
