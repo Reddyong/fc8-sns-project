eeb3718b246861316c0d1e44687c42d944bdb7f6
Reddyong <d0209hong@naver.com>
2024-07-29 22:54:42 +0900

    #28 - 알람 기능 로직 구현 완료.
    알람 리스트 조회 기능 구현.
    댓글을 달거나, 좋아요를 눌렀을때, 해당 사용자의 알람 저장.

---
+++ src/main/java/com/fc8/snsproject/domain/alarm/repository/AlarmRepository.java
@@ -0,0 +1,12 @@
+package com.fc8.snsproject.domain.alarm.repository;
+
+import com.fc8.snsproject.domain.alarm.entity.Alarm;
+import com.fc8.snsproject.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+
+    Page<Alarm> findAllByUser(User user, Pageable pageable);
+}
